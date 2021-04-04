fe31c92d095334dc3688862c598497c2e7e9fbf9
barun1607 <varunp2000@gmail.com>
2021-04-04 17:18:51 +0530

    added auth decorators to views

--- VirtualStocksDjango/stocks_api/views.py
+++ VirtualStocksDjango/stocks_api/views.py
@@ -1,65 +1,50 @@
-from django.shortcuts import get_object_or_404, render
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from django.http.response import JsonResponse
 from .stocksapi import *
 from .models import User
 from .serializers import UserSerializer, RegistrationSerializer
 from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-
-def stock(request, name):
-    data = get_stock_by_name(name)
-    return JsonResponse(data)
-
-
-def stocks(request):
-    data = get_stocks_list()
-    return JsonResponse({"data": data}, safe=False)
-
-
-def gainers(request):
-    data = get_gainers()
-    return JsonResponse(data, safe=False)
-
-
-def losers(request):
-    data = get_gainers()
-    return JsonResponse(data, safe=False)
-
-
-# def userApi(request, id=0):
-#     if request.method == 'GET':
-#         users = User.objects.all()
-#         users_serializer = UserSerializer(users, many=True)
-#         return JsonResponse(users_serializer.data, safe=False)
-
-#     elif request.method == 'POST':
-#         user_data = JSONParser().parse(request)
-#         users_serializer = UserSerializer(data=user_data)
-#         if users_serializer.is_valid():
-#             users_serializer.save()
-#             return JsonResponse("added successfully", safe=False)
-#         return JsonResponse("failed to add", safe=False)
-
-#     elif request.method == 'PUT':
-#         user_data = JSONParser().parse(request)
-#         user = User.objects.get(UserID=user_data['UserID'])
-#         user_serializer = UserSerializer(user, data=user_data)
-#         if user_serializer.is_valid():
-#             user_serializer.save()
-#             return JsonResponse("Updates Successfully!", safe=False)
-#         return JsonResponse("Failed to Update.", safe=False)
-
-#     elif request.method == "DELETE":
-#         user = User.objects.get(UserID=id)
-#         user.delete()
-#         return JsonResponse("Deleted successfully", safe=False)
 
 @api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def stock(request, name):
+    data = get_stock_by_name(name)
+    return Response(data)
+
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def stocks(request):
+    data = get_stocks_list()
+    return Response({"data": data}, safe=False)
+
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def gainers(request):
+    data = get_gainers()
+    return Response(data, safe=False)
+
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def losers(request):
+    data = get_gainers()
+    return Response(data)
+
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def apiHome(request):
     api_urls = {
         "Register a user": "register-user/",
@@ -71,6 +56,8 @@ def apiHome(request):
 
 
 @api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def listUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
@@ -91,6 +78,8 @@ def registerUser(request):
 
 
 @api_view(['DELETE'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def deleteUser(request, pk):
     user = get_object_or_404(User, UserID=pk)
     user.delete()
@@ -98,6 +87,8 @@ def deleteUser(request, pk):
 
 
 @api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def updateUser(request, pk):
     user = get_object_or_404(User, UserID=pk)
     serializer = UserSerializer(user, data=request.data)
