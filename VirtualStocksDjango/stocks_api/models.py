4c2d4c4461ac184b6d20cd322050a92f4af9a1fc
barun1607 <varunp2000@gmail.com>
2021-04-04 19:45:36 +0530

    minor changes

--- VirtualStocksDjango/stocks_api/models.py
+++ VirtualStocksDjango/stocks_api/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-from django.conf import settings
 from django.contrib import auth
 
 # Create your models here.
@@ -11,6 +10,9 @@ from django.contrib import auth
 class Stock(models.Model):
     ApiRef = models.CharField(max_length=50)
     StockID = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.ApiRef
 
 
 class Watchlists(models.Model):
