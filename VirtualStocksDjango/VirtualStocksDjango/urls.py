6b82984b88758f20fce466db29781091ef6df2b8
barun1607 <varunp2000@gmail.com>
2021-04-04 16:02:40 +0530

    added main urls

--- VirtualStocksDjango/VirtualStocksDjango/urls.py
+++ VirtualStocksDjango/VirtualStocksDjango/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token', views.obtain_auth_token),
+    path('api/', include('api.urls')),
 ]
