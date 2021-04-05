8e9dc8c9d68b5fa9ffb9cbbfe36d73452608eb50
barun1607 <varunp2000@gmail.com>
2021-04-05 18:42:59 +0530

    removed more useless imports

--- VirtualStocksDjango/stocks_api/views.py
+++ VirtualStocksDjango/stocks_api/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework import serializers, status
 from rest_framework.decorators import api_view
 from .stocksapi import *
 from .models import *
