b7e59a3276fe2f68e7f44c567b03478758a70a89
barun1607 <varunp2000@gmail.com>
2021-04-05 16:02:06 +0530

    Registered new models to admin

--- VirtualStocksDjango/stocks_api/admin.py
+++ VirtualStocksDjango/stocks_api/admin.py
@@ -3,6 +3,9 @@ from .models import *
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Watchlists)
+admin.site.register(WatchlistStocks)
+admin.site.register(Portfolios)
+admin.site.register(PortfolioStocks)
 admin.site.register(Stock)
 admin.site.register(Transactions)
 admin.site.register(Leaderboard)
