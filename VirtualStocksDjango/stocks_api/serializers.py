b4eb5073c03c726fa8c2835db4112ed381d6829d
barun1607 <varunp2000@gmail.com>
2021-04-07 13:15:44 +0530

    implemented sell stock serializer

--- VirtualStocksDjango/stocks_api/serializers.py
+++ VirtualStocksDjango/stocks_api/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 from .models import *
 from .stocksapi import get_stock_by_name
-from django.db.models import Sum
 from decimal import Decimal
 
 
@@ -136,7 +134,13 @@ class TransactStockSerializer(serializers.Serializer):
         quantity = self.validated_data['quantity']
 
         user = User.objects.get(UserID=uID)
-        stockDetails = get_stock_by_name(code)
+
+        try:
+            stockDetails = get_stock_by_name(code)
+        except AttributeError:
+            raise serializers.ValidationError({
+                "detail": "Stock api returned null response for the given stock code"
+            })
 
         price = Decimal(stockDetails['averagePrice'])
         balance = user.Usermoney
@@ -170,6 +174,86 @@ class TransactStockSerializer(serializers.Serializer):
         portfolioEntry.StockID = stock
         portfolioEntry.NumberOfStocks = quantity
         portfolioEntry.save()
+
+        portfolio = Portfolios.objects.get(
+            PortfolioID=user.PortfolioID.PortfolioID)
+
+        unrealizedValue = 0
+        transactAggregate = PortfolioStocks.objects.filter(
+            PortfolioID=user.PortfolioID.PortfolioID)
+
+        for entry in transactAggregate:
+            p = entry.TransactionID.Price
+            q = entry.NumberOfStocks
+            unrealizedValue += (p * q)
+        portfolio.UnrealizedValue = unrealizedValue
+        portfolio.save()
+
+    def sell(self):
+        uID = self.validated_data['UserID']
+        code = self.validated_data['code']
+        quantity = self.validated_data['quantity']
+
+        user = User.objects.get(UserID=uID)
+
+        try:
+            stockDetails = get_stock_by_name(code)
+        except AttributeError:
+            raise serializers.ValidationError({
+                "detail": "Stock api returned null response for the given stock code"
+            })
+
+        price = Decimal(stockDetails['averagePrice'])
+        balance = user.Usermoney
+
+        if not Stock.objects.filter(ApiRef=code).exists():
+            raise serializers.ValidationError({
+                "detail": "Stock code does not exist"
+            })
+
+        stock = Stock.objects.get(ApiRef=code)
+
+        if not PortfolioStocks.objects.filter(PortfolioID=user.PortfolioID.PortfolioID, StockID=stock.StockID).exists():
+            raise serializers.ValidationError({
+                "detail": "Your portfolio does not have this stock"
+            })
+
+        portfolioItems = PortfolioStocks.objects.filter(
+            PortfolioID=user.PortfolioID.PortfolioID, StockID=stock.StockID)
+
+        totalNoOfStocks = 0
+        for item in portfolioItems:
+            n = item.NumberOfStocks
+            totalNoOfStocks += n
+
+        if totalNoOfStocks < quantity:
+            raise serializers.ValidationError({
+                "detail": "There aren't enough stocks in your portfolio to carry out this request"
+            })
+
+        user.Usermoney = balance + (price * quantity)
+        user.save()
+
+        transaction = Transactions()
+        transaction.UserID = user
+        transaction.PortfolioID = user.PortfolioID
+        transaction.Price = price
+        transaction.Quantity = quantity
+        transaction.StockID = stock
+        transaction.isSold = True
+        transaction.save()
+
+        listLen = portfolioItems.__len__()
+        num = quantity
+        while num > 0:
+            if num >= portfolioItems[listLen - 1].NumberOfStocks:
+                num -= portfolioItems[listLen - 1].NumberOfStocks
+                portfolioItems[listLen - 1].delete()
+                listLen -= 1
+            else:
+                portfolioItems[listLen - 1].NumberOfStocks -= num
+                portfolioItems[listLen - 1].save()
+                num = 0
 
         portfolio = Portfolios.objects.get(
             PortfolioID=user.PortfolioID.PortfolioID)
